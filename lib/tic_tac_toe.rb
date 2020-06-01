dca063be5934b319077435d80fe2510aed6c8e5d
Teddy Bradsher <teddybradsher@gmail.com>
2020-06-01 04:48:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,6 +32,14 @@ def turn_count(board)
   end
   return turns
 end
+  def current_player (board)
+  turns = turn_count (board)
+  if turns % 2 == 0
+    return "X"
+  else 
+    return "O"
+  end
+end
   
   def play(board)
   action = 0
