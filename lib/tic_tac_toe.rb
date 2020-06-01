0aca84e5845331f2a05629aa6a063c56bab7c3af
Teddy Bradsher <teddybradsher@gmail.com>
2020-06-01 05:21:40 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -51,7 +51,7 @@ end
   
   def turn(board, input)
   puts "Please enter 1-9:"
-  input = gets.strip
+  input = gets.chomp
   index = input_to_index(input)
   if valid_move?(board, index)
     move(board, index, current_player)
