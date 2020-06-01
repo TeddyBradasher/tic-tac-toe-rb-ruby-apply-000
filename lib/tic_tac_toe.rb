78ae0b2058701f71f42d841b6b21bc720029eb42
Teddy Bradsher <teddybradsher@gmail.com>
2020-06-01 05:40:26 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,8 +49,8 @@ end
   end 
 end 
   
-  def turn(board, input)
-  puts "Please enter 1-9:"
+  def turn
+    puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index)
